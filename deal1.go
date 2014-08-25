557a69dcb601da72a48a188f0b496af72372b70b
gcrispin57 <gcrispin1@gmail.com>
2014-08-25 07:28:56 -0400

    added type Deck

--- deal1.go
+++ deal1.go
@@ -12,19 +12,29 @@ type Card struct {
 	Suit int
 }
 
-/*
-type Deck struct {
-	deck []Card
-//	Deal() []Card
+
+type Deck []Card
+
+func (deck Deck) initDeck() {
+	for i := 0; i < 13; i++ {
+		for j := 0; j < 4; j++ {
+			deck[j*13 + i] = Card{i, j}
+		}
+	}
+	deck.shuffle()
 }
 
-func (deck Deck) NewDeck() {
-	deck = {Card{0,0}, Card{0,1}}
-//	return deck
+func (deck Deck) printDeck() {
+	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
+							 4:"6",5:"7",6:"8",7:"9",
+							 8:"T",9:"J",10:"Q",11:"K",12:"A"}
+	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
+	for _, card := range deck {
+		fmt.Printf("%s%s ", rankP[card.Rank], suitP[card.Suit])
+	}
 }
-*/
 
-func shuffle(deck []Card ) {
+func (deck Deck) shuffle() {
 	var rnd int
 	for i := range deck {
 		rnd = rand.Intn(51)
@@ -32,24 +42,34 @@ func shuffle(deck []Card ) {
 	}
 }
 
-func main() {
-	rand.Seed( time.Now().UTC().UnixNano())
-	deck := make([] Card, 52)
+
+/*
+func New(deck interface{}) (Deck error) {
+		deck = make([] Card, 52)
 	for i := 0; i < 13; i++ {
 		for j := 0; j < 4; j++ {
 			deck[j*13 + i] = Card{i, j}
 		}
 	}
-	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
-							 4:"6",5:"7",6:"8",7:"9",
-							 8:"T",9:"J",10:"Q",11:"K",12:"A"}
-	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
-	shuffle(deck)
-	for _, card := range deck {
-		fmt.Printf("%s%s ", rankP[card.Rank], suitP[card.Suit])
-	}
+}
+
+
+func (deck Deck) NewDeck() {
+	deck = {Card{0,0}, Card{0,1}}
+//	return deck
+}
+*/
+
+func main() {
+	rand.Seed( time.Now().UTC().UnixNano())
+//	deck := make([] Card, 52)
+	var deck Deck
+	deck = make([]Card, 52)
+	deck.initDeck()
+	deck.printDeck()
+}
 
 
 //	deck.Deal()
 //	deck.Print()
-}
+
