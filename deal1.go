00cdce36c530e1a30ba1145352bce918b2458232
gcrispin57 <gcrispin1@gmail.com>
2014-08-25 07:59:46 -0400

    modofied type Deck struct - Deck is now a struct

--- deal1.go
+++ deal1.go
@@ -13,32 +13,35 @@ type Card struct {
 }
 
 
-type Deck []Card
-
-func (deck Deck) initDeck() {
-	for i := 0; i < 13; i++ {
-		for j := 0; j < 4; j++ {
-			deck[j*13 + i] = Card{i, j}
-		}
-	}
-	deck.shuffle()
+type Deck struct {
+	deck []Card
+	ind int // points to "top" of deck
 }
 
-func (deck Deck) printDeck() {
+func (d Deck) initDeck() {
+	for i := 0; i < 13; i++ {
+		for j := 0; j < 4; j++ {
+			d.deck[j*13 + i] = Card{i, j}
+		}
+	}
+	d.shuffle()
+}
+
+func (d Deck) printDeck() {
 	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
 							 4:"6",5:"7",6:"8",7:"9",
 							 8:"T",9:"J",10:"Q",11:"K",12:"A"}
 	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
-	for _, card := range deck {
+	for _, card := range d.deck {
 		fmt.Printf("%s%s ", rankP[card.Rank], suitP[card.Suit])
 	}
 }
 
-func (deck Deck) shuffle() {
+func (d Deck) shuffle() {
 	var rnd int
-	for i := range deck {
+	for i := range d.deck {
 		rnd = rand.Intn(51)
-		deck[i], deck[rnd] = deck[rnd], deck[i]
+		d.deck[i], d.deck[rnd] = d.deck[rnd], d.deck[i]
 	}
 }
 
@@ -63,10 +66,10 @@ func (deck Deck) NewDeck() {
 func main() {
 	rand.Seed( time.Now().UTC().UnixNano())
 //	deck := make([] Card, 52)
-	var deck Deck
-	deck = make([]Card, 52)
-	deck.initDeck()
-	deck.printDeck()
+	var d Deck
+	d.deck = make([]Card, 52)
+	d.initDeck()
+	d.printDeck()
 }
 
 
