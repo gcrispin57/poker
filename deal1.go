8980931c86bc79f556369a83733ebf810b56f554
gcrispin57 <gcrispin1@gmail.com>
2014-09-12 05:40:39 -0400

    generating hand combinations, but not saving them properly

--- deal1.go
+++ deal1.go
@@ -27,7 +27,11 @@ func (d Deck) initDeck() {
 	d.shuffle()
 }
 
-func (d Deck) printDeck(rankP, suitP map[int]string) {
+func (d Deck) printDeck() {
+	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
+			 4:"6",5:"7",6:"8",7:"9",
+			 8:"T",9:"J",10:"Q",11:"K",12:"A"}
+	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
 	fmt.Printf("%s", "Deck: ")
 	for _, card := range d.cards {
 		fmt.Printf("%s%s ", rankP[card.Rank], suitP[card.Suit])
@@ -43,12 +47,19 @@ func (d Deck) shuffle() {
 	}
 }
 
+
 type Hand struct {
 	cards []Card
 	name string
+	attr Attributes
 }
 
-func (h Hand) printHand(rankP, suitP map[int]string) {
+func (h Hand) printHand() {
+	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
+			 4:"6",5:"7",6:"8",7:"9",
+			 8:"T",9:"J",10:"Q",11:"K",12:"A"}
+	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
+
 	fmt.Printf("%s: ", h.name)
 	for _, card := range h.cards {
 		fmt.Printf("%s%s ", rankP[card.Rank], suitP[card.Suit])
@@ -60,6 +71,27 @@ type Players []Hand
 
 type Dealer Hand
 
+type Cards []Card
+
+type Combo []Cards
+
+
+type Attributes struct {
+	numCl			int	//number of cards in each suit
+	numDi			int
+	numHe			int
+	numSp			int
+	hasStrFlush		bool
+	has4Kind		bool
+	hasFullHouse	bool
+	hasFlush		bool
+	hasStraight		bool
+	hasTrips		bool
+	hasTwoPair		bool
+	hasPair 		bool
+	hasNoPair		bool
+	handResult		string
+}
 /*
 func New(deck interface{}) (Deck error) {
 		deck = make([] Card, 52)
@@ -81,11 +113,6 @@ func main() {
 	rand.Seed( time.Now().UTC().UnixNano())
 	numPlayers := 5
 
-	rankP := map[int] string{0:"2",1:"3",2:"4",3:"5",
-			 4:"6",5:"7",6:"8",7:"9",
-			 8:"T",9:"J",10:"Q",11:"K",12:"A"}
-	suitP := map[int] string{0:"C",1:"D",2:"H",3:"S"}
-
 //	deck := make([] Card, 52)
 	var d Deck
 	d.cards = make([]Card, 52)
@@ -99,13 +126,124 @@ func main() {
 		h.name = "player " + strconv.Itoa(i)
 		pl[i] = h
 	}
-	dealGame(d, pl, numPlayers, rankP, suitP)
+	var dealer Hand
+	dealer = dealGame(d, pl, dealer, numPlayers)
+	evaluate(d, pl, dealer, numPlayers)
+}
+func evaluate(d Deck, pl Players, dealer Hand, numPlayers int) {
+	var numSp, numHe, numDi, numCl int
+	for card := range dealer.cards {
+		switch dealer.cards[card].Suit {
+		case 0: numCl++
+		case 1: numDi++
+		case 2: numHe++
+		case 3: numSp++
+		}
+	}
+	dealer.attr.numSp = numSp
+	dealer.attr.numHe = numHe
+	dealer.attr.numDi = numDi
+	dealer.attr.numCl = numCl		
+	fmt.Println(dealer.attr)
+	for hand := range pl {
+		numSp, numHe, numDi, numCl := 0, 0, 0, 0		
+		for card := range pl[hand].cards {
+			switch pl[hand].cards[card].Suit {
+			case 0: numCl++
+			case 1: numDi++
+			case 2: numHe++
+			case 3: numSp++
+			}
+		}
+		pl[hand].attr.numSp = numSp
+		pl[hand].attr.numHe = numHe
+		pl[hand].attr.numDi = numDi
+		pl[hand].attr.numCl = numCl
+		//Determine hand value
+		
+		var possibleHands []Cards
+		possibleHands = genHands(pl[hand], 2, dealer, 3)
+		fmt.Println(possibleHands)
+	}
+}
+/*
+		select {
+		case hasStrFlush(pl[hand], dealer):
+			pl[hand].attr.handResult = "Straight Flush"
+		case hasFullHouse(pl[hand], dealer):
+			pl[hand].attr.handResult = "Full House"
+		case hasFlush(pl[hand], dealer):
+			pl[hand].attr.handResult = "Flush" 
+		case hasStraight(pl[hand], dealer):
+			pl[hand].attr.handResult = "Straight" 
+		case hasTrips(pl[hand], dealer):
+			pl[hand].attr.handResult = "Three of a kind" 
+		case hasTwoPair(pl[hand], dealer): 
+			pl[hand].attr.handResult = "Two Pair" 
+		case hasPair(pl[hand], dealer):
+			pl[hand].attr.handResult = "Pair" 
+		case hasNoPair(pl[hand], dealer): 
+			pl[hand].attr.handResult = "Nothing" }
+		fmt.Println(pl[hand].attr)
+	}
+*/
+
+/*
+type Players []Hand
+
+type Dealer Hand
+
+type Cards []Card
+
+type  []Cards []Cards
+*/
+
+func genHands(h1 Hand, numh1 int, h2 Hand, numh2 int )  []Cards {
+	if (len(h1.cards) < numh1 || len(h2.cards) < numh2) {
+		panic(fmt.Sprintf("%d %d Invalid integer parameter", numh1, numh2))
+	}
+	combo1 := combinations(h1, numh1)
+	combo2 := combinations(h2, numh2)
+	fmt.Println(combo1)
+	fmt.Println(combo2)
+	return combine(combo1, combo2)
+}
+
+func combine(combo1  []Cards, combo2  []Cards) []Cards { // to write, combine all possible combinations together
+	var allCombos  []Cards
+	return allCombos
+}
+
+func combinations(h Hand, r int)  []Cards {
+	//Generates all combinations of hand h, choosing r (nCr)
+	n := len(h.cards)	 
+	var combos []Cards 	//total number of possible combinations of r (nCr) for 5C3
+	hand := make(Cards, r)	//individual generated hands
+	fmt.Printf("Entering combinationsUtil from combinations - hand= ")
+	h.printHand()
+	return combinationsUtil(h, combos, hand, 0, n-1, 0, r)
+}
+
+func combinationsUtil(h Hand, combos []Cards, hand Cards, start, end, index, r int) []Cards {
+
+//	if start == 0 { indPos = 0 }
+	if index == r { //hand has been generated
+		combos = append(combos, hand)
+		fmt.Println("Combos: ",combos)
+		return combos
+	}
+
+	for i := start; i <= end && end-i+1 >= r-index; i++ {
+//		fmt.Printf("i= %d start= %d end= %d index= %d r= %d\n", i, start, end, index, r)
+	   	hand[index] = h.cards[i]
+    	combinationsUtil(h, combos, hand, i+1, end, index+1, r)
+    }
+    return combos
 }
 
 
-func dealGame(d Deck, pl Players, numPlayers int, rankP, suitP map[int]string) {
+func dealGame(d Deck, pl Players, dealer Hand, numPlayers int) Hand {
 	//deal dealer
-	var dealer Hand
 	dealer.name = "Dealer"
 	dealer.cards = d.cards[d.ind:d.ind+5]
 	d.ind += 5
@@ -114,15 +252,16 @@ func dealGame(d Deck, pl Players, numPlayers int, rankP, suitP map[int]string) {
 		pl[hand].cards = d.cards[d.ind:d.ind+4]
 		d.ind += 4
 	}
-	displayGame(d, pl, dealer, rankP, suitP)
+	displayGame(d, pl, dealer)
+	return dealer
 }
 
-func displayGame(d Deck, pl Players, dealer Hand, rankP, suitP map[int]string) {
-	d.printDeck(rankP, suitP)
-	dealer.printHand(rankP, suitP)
+func displayGame(d Deck, pl Players, dealer Hand) {
+	d.printDeck()
+	dealer.printHand()
 	//print Player's hands
 	for _, player := range pl {
-		player.printHand(rankP, suitP)
+		player.printHand()
 	}
 }
 
